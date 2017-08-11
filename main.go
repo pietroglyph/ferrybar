f0782c72e72a03a39b03ba6d14576c546db2f2ec
Declan Freeman-Gleason <declanfreemangleason@gmail.com>
2017-08-10 22:37:52 -0700

    Added the beginnings of the > 2 point path progress computing.

--- main.go
+++ main.go
@@ -10,7 +10,7 @@ import (
 
 type config struct {
 	apiKey                string
-	targetTerminal        int
+	departingTerminal     int
 	updateFrequency       int
 	vesselEndpointBaseURL string
 }
@@ -18,7 +18,7 @@ type config struct {
 func main() {
 	var conf config
 	flag.StringVarP(&conf.apiKey, "key", "k", "", "WSDOT Traveller Information API key (provisioned at http://wsdot.wa.gov/traffic/api/)") // Required flag
-	flag.IntVarP(&conf.targetTerminal, "terminals", "t", 3,
+	flag.IntVarP(&conf.departingTerminal, "terminals", "t", 3,
 		"Departing Terminal ID to get boat progress for, terminal list avaliable by making a GET to http://www.wsdot.wa.gov/ferries/api/terminals/rest/terminalbasics?apiaccesscode={CODE}") // 3 is the Bainbridge Island Ferry terminal
 	flag.IntVarP(&conf.updateFrequency, "update", "u", 60, "The frequency of GETs to the /vessellocations endpoint, in seconds")
 	flag.StringVarP(&conf.vesselEndpointBaseURL, "baseurl", "b", "http://www.wsdot.wa.gov/ferries/api/vessels/rest", "The URL of the WSDOT REST API vessel data endpoint")
@@ -27,6 +27,11 @@ func main() {
 	// apiKey flag is required
 	if conf.apiKey == "" {
 		log.Fatal("Please specify an API using the -k flag.")
+	}
+
+	// We only have the ferryPathPoints for the Seattle-Bainbridge route
+	if conf.departingTerminal != 3 || conf.departingTerminal != 7 {
+		log.Fatal("Processing location data is only implimented for Departing Terminal ID 3 and 7")
 	}
 
 	fmt.Println("Flags parsed.")
@@ -40,7 +45,7 @@ func main() {
 	// Go doesn't have a monotonic time source, so we'll use this for measuring duration
 	lastUpdate := time.Now()
 
-	var locationData vesselLocation
+	var processedLocationData processedVesselLocation
 	for {
 		if time.Since(lastUpdate).Seconds() >= float64(conf.updateFrequency) {
 			// We update the progess concurrently with this HTTP request to the endpoint
@@ -54,12 +59,12 @@ func main() {
 				log.Println("Recieved empty location data; trying again in ", conf.updateFrequency, " seconds")
 			} else {
 				log.Println("Location data updated sucsessfully")
-				locationData = locData
+				processedLocationData = locData.process()
 			}
 			lastUpdate = time.Now()
 		default: // For this to be non-blocking the default clause is required
 			break
 		}
-		locationData.progress()
+		fmt.Println(processedLocationData.progress())
 	}
 }
--- process.go
+++ process.go
@@ -1,8 +1,44 @@
 package main
 
-import "fmt"
+type processedVesselLocation struct {
+	pathIndex int
+}
 
-func (v *vesselLocation) progress() int {
-	fmt.Println(v.Eta.Minute())
+type coordinate struct {
+	X, Y float32
+}
+
+// ferryPathPoints are latitude and longitude of the Seattle-Bainbridge route, sourced from Google Maps
+// We interpolate between these points linearly for the time being, but a spline function
+// could be introduced if we need the extra accuracy (which I don't think we need)
+var ferryPathPoints = []coordinate{ // XXX: why does golint yell at me about this?
+	{47.602824, -122.339544},
+	{47.602869, -122.342291},
+	{47.603069, -122.343750},
+	{47.604169, -122.352440},
+	{47.605326, -122.482388},
+	{47.605471, -122.484770},
+	{47.605688, -122.486615},
+	{47.605934, -122.487774},
+	{47.606353, -122.488804},
+	{47.606643, -122.489362},
+	{47.607163, -122.490134},
+	{47.607757, -122.490735},
+	{47.608176, -122.491014},
+	{47.617116, -122.494469},
+	{47.617825, -122.494855},
+	{47.618331, -122.495220},
+	{47.619170, -122.496078},
+	{47.619546, -122.496700},
+	{47.620009, -122.497602},
+	{47.620197, -122.498288},
+	{47.622453, -122.509274},
+}
+
+func (v *vesselLocation) process() processedVesselLocation {
+	return processedVesselLocation{}
+}
+
+func (v *processedVesselLocation) progress() int {
 	return 0
 }
