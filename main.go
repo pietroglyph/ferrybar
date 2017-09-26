15bb28f48b7845c12212d29dceb114fa4133c0fc
Declan Freeman-Gleason <declanfreemangleason@gmail.com>
2017-09-25 20:07:29 -0700

    Changed endpoint URLs to https, format numbers at uniform decimal places for display, and fixed a comment.

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"log"
+	"math/big"
 	"os"
 	"strings"
 	"time"
@@ -23,9 +24,9 @@ type config struct {
 func main() {
 	var conf config
 	// Get command line flags
-	flag.StringVarP(&conf.apiKey, "key", "k", "", "WSDOT Traveller Information API key (provisioned at http://wsdot.wa.gov/traffic/api/)") // REquired
-	flag.IntVarP(&conf.terminal, "terminal", "t", 3,                                                                                       // 3 is the Bainbridge Island Ferry terminal
-		"Terminal ID to get arriving and departing boat progress for. A terminal list is available by making a GET to http://www.wsdot.wa.gov/ferries/api/terminals/rest/terminalbasics?apiaccesscode={CODE}")
+	flag.StringVarP(&conf.apiKey, "key", "k", "", "WSDOT Traveller Information API key (provisioned at https://wsdot.wa.gov/traffic/api/)") // Required
+	flag.IntVarP(&conf.terminal, "terminal", "t", 3,                                                                                        // 3 is the Bainbridge Island Ferry terminal
+		"Terminal ID to get arriving and departing boat progress for. A terminal list is available by making a GET to https://www.wsdot.wa.gov/ferries/api/terminals/rest/terminalbasics?apiaccesscode={CODE}")
 	flag.IntVarP(&conf.updateFrequency, "update", "u", 60, "The frequency of GETs to the /vessellocations endpoint, in seconds")
 	flag.StringVarP(&conf.vesselEndpointBaseURL, "baseurl", "b", "http://www.wsdot.wa.gov/ferries/api/vessels/rest", "The URL of the WSDOT REST API vessel data endpoint")
 	flag.Float64VarP(&conf.routeWidthFactor, "width", "w", 300, "The 'width' factor of the route, this determines how far away the ferry can be to still be considered on route")
@@ -85,7 +86,6 @@ func main() {
 			}
 
 			// Delete all location data for vessels not departing or arriving from the target terminal
-			log.Println("Location data updated sucsessfully")
 			for i := 0; i < len(locations); i++ {
 				if locations[i].DepartingTerminalID != conf.terminal && locations[i].ArrivingTerminalID != conf.terminal {
 					locations = append(locations[:i], locations[i+1:]...)
@@ -102,7 +102,7 @@ func main() {
 			if v.AtDock || !v.InService {
 				fmt.Print(v.VesselName, " is currently docked\n")
 			} else {
-				fmt.Print(v.VesselName+": ", v.process(&conf), " percent to ", v.ArrivingTerminalName, "\n")
+				fmt.Print(v.VesselName+": ", big.NewFloat(v.process(&conf)*100).SetMode(big.AwayFromZero).Text('f', 4), " percent to ", v.ArrivingTerminalName, "\n")
 			}
 			fmt.Println("Data was last changed at", v.TimeStamp.String())
 		}
